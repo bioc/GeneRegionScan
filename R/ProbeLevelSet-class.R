71784643f397a014167c5602149f64e64b0d5cec
Patrick Aboyoun <unknown>
2009-06-18 00:00:30 +0000

    Added scanDate slot to Biobase's eSet class, added functionality in affy to utilize the new scanDate slot and updated all the packages that create direct subclasses of eSet, ExpressionSet or NChannelSet.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GeneRegionScan@40196 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ProbeLevelSet-class.R
+++ R/ProbeLevelSet-class.R
@@ -1,23 +1,5 @@
 setClass("ProbeLevelSet", contains="ExpressionSet")
 
-#
-setMethod("initialize", "ProbeLevelSet",
-function(.Object,
-    #phenoData = new("AnnotatedDataFrame"),
-    #featureData = new("AnnotatedDataFrame"),
-    #experimentData = new("MIAME"),
-    #annotation = character(),
-    ...
-) {
-    callNextMethod(.Object,
-        #phenoData = phenoData,
-        #featureData = featureData,
-        #experimentData = experimentData,
-        #annotation = annotation,
-        ...
-    )
-})
-
 setValidity("ProbeLevelSet", function(object) {
     assayDataValidMembers(assayData(object))
 })
